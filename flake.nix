ec53601e07fc0e0e287ad283a9c5a189fb462baf
niten <niten@fudo.org>
2025-08-10 19:03:53 -0700

    Initial checkin

---
+++ flake.nix
@@ -0,0 +1,96 @@
+{
+  description = "Deployment using deploy-rs";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    utils.url = "github:numtide/flake-utils";
+
+    fudo-nixos = {
+      url = "path:/state/deploy/config";
+      inputs = {
+        fudo-entities.follows = "fudo-entities";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    fudo-entities = {
+      url = "git+ssh://git@github.com/fudoniten/fudo-entities";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, utils, deploy-rs, fudo-nixos, fudo-entities, ... }@inputs:
+    with nixpkgs.lib;
+    let
+      defaultSshOpts = [ "-oControlMaster=no" "-oControlPath=none" ];
+
+      allNodes = let
+        nodeEntities = filterAttrs (_: hostOpts: hostOpts.deploy.enable)
+          fudo-entities.entities.hosts;
+      in mapAttrs (hostname: hostOpts: {
+        hostname = fudo-entities.lib.getHostIpv4 hostname;
+        sshOpts = defaultSshOpts ++ hostOpts.deploy.ssh-options;
+        sshUser = "root";
+
+        inherit (hostOpts) site domain;
+
+        profiles.system = {
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos
+            fudo-nixos.nixosConfigurations."${hostname}";
+        };
+      }) nodeEntities;
+
+      domains = fudo-entities.entities.domains;
+      sites = fudo-entities.entities.sites;
+
+    in utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells = rec {
+          default = deploy;
+          deploy = pkgs.mkShell {
+            buildInputs = [ deploy-rs.packages."${system}".deploy-rs ];
+          };
+        };
+      }) // (mapAttrs' (domainName: _:
+        nameValuePair "domain-${domainName}" {
+          type = "deploy";
+
+          autoRollback = true;
+          magicRollback = true;
+
+          nodes =
+            filterAttrs (_: hostOpts: hostOpts.domain == domainName) allNodes;
+        }) domains) // (mapAttrs' (siteName: _:
+          nameValuePair "site-${siteName}" {
+            type = "deploy";
+
+            autoRollback = true;
+            magicRollback = true;
+
+            nodes =
+              filterAttrs (_: hostOpts: hostOpts.site == siteName) allNodes;
+          }) sites) // {
+            deploy = with nixpkgs.lib; {
+              type = "deploy";
+
+              autoRollback = true;
+              magicRollback = true;
+              fastConnection = true;
+
+              nodes = allNodes;
+            };
+
+            checks = builtins.mapAttrs
+              (system: deployLib: deployLib.deployChecks self.deploy)
+              deploy-rs.lib;
+          };
+}
